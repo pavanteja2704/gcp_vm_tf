c393b252a014cb34256d7fcc7f4132d60708d832
Pavan Teja <yellaboyinapavanteja@gmail.com>
2026-06-17 13:13:46 +0530

    hide

--- vm_template/variables.tf
+++ vm_template/variables.tf
@@ -3,9 +3,9 @@ variable "vm" {
   sensitive = false
 }
 
-variable "access_token" {
-  type = string
-}
+#variable "access_token" {
+# type = string
+#}
 
 variable "project_id" {
   type    = string
